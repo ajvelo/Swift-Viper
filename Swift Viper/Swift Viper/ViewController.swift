4ccaaec0054fe5ab111dd309dc94097a9d4b1de4
Andreas Velounias <andreas.velounias@weswap.com>
2019-12-16 14:03:57 +0000

    Init Xcode project

---
+++ Swift Viper/Swift Viper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Viper
+//
+//  Created by Andreas Velounias on 16/12/2019.
+//  Copyright © 2019 Andreas Velounias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
