599a7e252a6bc57957cfeb6e545e8e99790c4fc0
ajvelo <>
2019-12-16 14:21:27 +0000

    Add module builder

---
+++ Swift Viper/Swift Viper/HomeModule/HomeModuleBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  HomeModuleBuilder.swift
+//  Swift Viper
+//
+//  Created by Andreas Velounias on 16/12/2019.
+//  Copyright © 2019 Andreas Velounias. All rights reserved.
+//
+
+import UIKit
+
+// Create the dependencies between the different components
+class HomeModuleBuilder {
+    
+    func build() -> UIViewController {
+        
+    }
+    
+}
